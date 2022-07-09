bfa1f72f7369debc6e02fe2654d5eb8e9ec66ed2
John Ahn <smileajw1004@gmail.com>
2022-07-09 23:13:58 +0900

    initial

---
+++ react-mobx-counter-app(1)/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import counterStore from './store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 인스턴스 생성
+const store = new counterStore();
+
+root.render(
+  <React.StrictMode>
+    <App myCounter={store} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ react-mobx-counter-context-app(3)/src/App.js
@@ -0,0 +1,18 @@
+import { observer } from 'mobx-react';
+import { useCounterStore } from './context/counterContext';
+
+const App = observer(() => {
+  const myCounter = useCounterStore();
+  return (
+    <div style={{ textAlign: 'center', padding: 16 }}>
+      카운트: {myCounter.count}
+      <br /><br />
+      마이너스?: {myCounter.isNegative}
+      <br /><br />
+      <button onClick={() => myCounter.increase()}>+</button>
+      <button onClick={() => myCounter.decrease()}>-</button>
+    </div>
+  )
+});
+
+export default App;
